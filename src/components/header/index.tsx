fa567682a3f8064f797e2c6a042afc02d60ccf89
NathanScott85 <contact@nathanscott.net>
2020-04-22 18:57:47 +0100

    updated git ignore, index.html, created header component

---
+++ src/components/header/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './header.css';
+
+export const Header = () => {
+    return (
+        <div className={`qa-company-header-container`}><div className={`qa-sign-up`}>Sign Up</div><div className={`qa-login`}>Login</div><h1 className={`qa-company-name`}>Sahara</h1></div>
+    );
+}
